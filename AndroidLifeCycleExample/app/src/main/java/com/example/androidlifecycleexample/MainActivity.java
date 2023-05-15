57f73a6c287d2459a710504c2f7ec1c61bbb19d5
towhidulislam99 <towhidulislam618@gmail.com>
2023-05-15 07:46:56 -0700

    Modified and add Two New Method

--- AndroidLifeCycleExample/app/src/main/java/com/example/androidlifecycleexample/MainActivity.java
+++ AndroidLifeCycleExample/app/src/main/java/com/example/androidlifecycleexample/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.androidlifecycleexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et = findViewById(R.id.ET1);
+
+
         Log.d("Life Cycle", "onCreate: ");
 
     }
@@ -19,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         Log.d("Life Cycle", "onStart: ");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d("Life Cycle", "onRestoreInstanceState: ");
+        String s = savedInstanceState.getString("TEXT");
+        et.setText(s);
     }
 
     @Override
@@ -31,6 +48,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         Log.d("Life Cycle", "onPause: ");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        Log.d("Life Cycle", "onSaveInstanceState: ");
+        String input =et.getText().toString();
+        outState.putString("TEXT",input);
     }
 
     @Override
